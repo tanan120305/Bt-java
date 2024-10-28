50ba916c3881cfb10e8aa41406346230d34adb03
doem5 <doem5@MSI>
2024-10-29 00:36:43 +0700

    bt java

---
+++ src/bt/java/BtJava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bt.java;
+
+/**
+ *
+ * @author doem5
+ */
+public class BtJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Huy Tan An");
+        System.out.println("CMU TPM15");
+        System.out.println("Huy Tan An");
+    }
+    
+}
